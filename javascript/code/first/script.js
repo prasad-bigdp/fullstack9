ecfc6e0ac26c5412cdf238c034c7c1b237663373
prasad-bigdp <bigdp585@gmail.com>
2024-12-17 10:34:15 +0530

    message

---
+++ javascript/code/first/script.js
@@ -0,0 +1,52 @@
+/* muti 
+ine 
+comments*/
+console.log("bye world")
+// use muti line commands, external JS  and prints Hello world
+//var
+function fun ()
+{
+    if (true)
+    {
+        y = 25; 
+    }
+    console.log(y)
+}
+fun()
+
+var x = 25;
+var x = 35;  //redclare is possible
+var y = 30;
+y = 45; //reassign
+
+//let 
+function fun ()
+{
+    if (true)
+    {
+      let  y = 25; 
+    }
+    console.log(y)
+}
+fun()
+
+let x1 = 25;
+ x1 = 35;  //redclare is not possible
+let z = 30;
+z = 45; //reassign
+
+//const 
+function fun ()
+{
+    if (true)
+    {
+      const  y = 25; 
+    }
+    console.log(y)
+}
+fun()
+/*
+const x11 = 25;
+ x11 = 35;  //redclare is not possible
+const z1 = 30;
+z1 = 45; //reassign*/
