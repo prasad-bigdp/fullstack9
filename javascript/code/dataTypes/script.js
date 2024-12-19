21005e398282d048daf5b8b8fec7dcd5c6d5b7f2
prasad-bigdp <bigdp585@gmail.com>
2024-12-19 10:41:52 +0530

    message

---
+++ javascript/code/dataTypes/script.js
@@ -0,0 +1,22 @@
+let x = 25;
+console.log(typeof (x))
+x = "raj"
+console.log(typeof x)
+c = 25.638145;
+console.log(typeof c, c.toFixed(2)) //25.64
+c = 25.638145
+console.log(typeof c, c.toPrecision(3)) //25.6
+c = 25;
+console.log(c.toString(2))
+console.log(Math.max(2, 3, 5, 6, 7, 8, 1, 2)) //8
+console.log(Math.min(5, 4, 7, 1, 2)) //1
+console.log(Math.sqrt(4))//2
+console.log(Math.cbrt(27))//3
+console.log(Math.abs(-10))//10
+console.log(Math.ceil(3.14))// nearest larger integer -4
+console.log(Math.floor(3.8)) // nearest smaller integer -3
+console.log(Math.ceil(-2.45)) // -2
+console.log(Math.round(2.4))
+console.log(Math.pow(2, 3), 2 ** 3) //8
+console.log(Math.ceil(Math.random() * 10000))
+console.log(Math.ceil(Math.random() * 6))
