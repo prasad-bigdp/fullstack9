bee7858a5d9ebb0f3cbbe02ef4c15f75de3b2946
prasad-bigdp <bigdp585@gmail.com>
2024-12-23 10:32:52 +0530

    update

--- javascript/code/dataTypes/script.js
+++ javascript/code/dataTypes/script.js
@@ -1,54 +1,66 @@
-let x = Infinity;
-console.log(typeof (x))
-x = "raj"
-console.log(typeof x)
-c = 25.638145;
-console.log(typeof c, c.toFixed(2)) //25.64
-c = 25.638145
-console.log(typeof c, c.toPrecision(3)) //25.6
-c = 25;
-console.log(c.toString(2))
-console.log(Math.max(2, 3, 5, 6, 7, 8, 1, 2)) //8
-console.log(Math.min(5, 4, 7, 1, 2)) //1
-console.log(Math.sqrt(4))//2
-console.log(Math.cbrt(27))//3
-console.log(Math.abs(-10))//10
-console.log(Math.ceil(3.14))// nearest larger integer -4
-console.log(Math.floor(3.8)) // nearest smaller integer -3
-console.log(Math.ceil(-2.45)) // -2
-console.log(Math.round(2.4))
-console.log(Math.pow(2, 3), 2 ** 3) //8
-console.log(Math.ceil(Math.random() * 10000))
-console.log(Math.ceil(Math.random() * 6))
+console.log(a) //Hoisting
+var a = 52;
+console.log(a)
+
+function abc ()
+{
+    console.log(k)
+    var k = 20;
+    console.log("i'm a function")
+}
+abc()
+
+// let x = Infinity;
+// console.log(typeof (x))
+// x = "raj"
+// console.log(typeof x)
+// c = 25.638145;
+// console.log(typeof c, c.toFixed(2)) //25.64
+// c = 25.638145
+// console.log(typeof c, c.toPrecision(3)) //25.6
+// c = 25;
+// console.log(c.toString(2))
+// console.log(Math.max(2, 3, 5, 6, 7, 8, 1, 2)) //8
+// console.log(Math.min(5, 4, 7, 1, 2)) //1
+// console.log(Math.sqrt(4))//2
+// console.log(Math.cbrt(27))//3
+// console.log(Math.abs(-10))//10
+// console.log(Math.ceil(3.14))// nearest larger integer -4
+// console.log(Math.floor(3.8)) // nearest smaller integer -3
+// console.log(Math.ceil(-2.45)) // -2
+// console.log(Math.round(2.4))
+// console.log(Math.pow(2, 3), 2 ** 3) //8
+// console.log(Math.ceil(Math.random() * 10000))
+// console.log(Math.ceil(Math.random() * 6))
 
 //strings
 // let str = 'The gandhi said, "good morning"'
-let str= "Time is 10 o' clock"
-console.log(str)
-let a = 5, b = 20;
+// let str= "Time is 10 o' clock"
+// console.log(str)
+// let a = 5, b = 20;
 // print in console , The sum of 10 and 20 is 30
-console.log("The sum of " + a + " and " + b + " is " + (a + b))
-console.log(`The sum of  ${a} and ${b}  is  ${(a + b)}`)
-let cost = 100, quantity = 4
+// console.log("The sum of " + a + " and " + b + " is " + (a + b))
+// console.log(`The sum of  ${a} and ${b}  is  ${(a + b)}`)
+// let cost = 100, quantity = 4
 // print in console, The total cost if 4 apples with each price of ₹100 is ₹400*/
-console.log(
-    `The total cost of ${quantity} with each price of ₹${cost}  is ₹${cost * quantity}`,
-)
+// console.log(
+//     `The total cost of ${quantity} with each price of ₹${cost}  is ₹${cost * quantity}`,
+// )
 
-str = "javascript";
-console.log(str.length) //10
-console.log(str.toUpperCase()) //JAVASCRIPT
-console.log(str)
-console.log(str.toLowerCase()) //javascript
-console.log(str.startsWith('ja')) //T
-console.log(str.endsWith('pt')) //T
-console.log(str.charAt(2))
-console.log(str.indexOf('a'))
-console.log(str.lastIndexOf("a"))
-console.log(str.concat(" is easy"))
-console.log("javascript is easy".split(''));
-console.log(str.slice(2, 5))//vas
-console.log("    prasad    ".trim())
+// str = "javascript";
+// console.log(str.length) //10
+// console.log(str.toUpperCase()) //JAVASCRIPT
+// console.log(str)
+// console.log(str.toLowerCase()) //javascript
+// console.log(str.startsWith('ja')) //T
+// console.log(str.endsWith('pt')) //T
+// console.log(str.charAt(2))
+// console.log(str.indexOf('a'))
+// console.log(str.lastIndexOf("a"))
+// console.log(str.concat(" is easy"))
+// console.log("javascript is easy".split(''));
+// console.log(str.slice(2, 5))//vas
+// console.log("    prasad    ".trim())
 
 
 
