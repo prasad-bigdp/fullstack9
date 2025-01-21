3ed1697a612bb46cb77d5a8fd8561fe4abaff0d1
prasad-bigdp <bigdp585@gmail.com>
2025-01-21 10:41:10 +0530

    update

---
+++ javascript/code/timers/script.js
@@ -0,0 +1,49 @@
+// let c = document.getElementById('count')
+// let count = 10;
+// function myFun2 ()
+// {
+//      count--
+// 	c.textContent = count
+// }
+// function myFun ()
+// {
+//     document.getElementById('greeting').textContent="Bye"
+// }
+// setTimeout(myFun, 5000)
+// let myInterval=setInterval(myFun2, 1000)
+// document.getElementById('btn').addEventListener('click', function ()
+// {
+//     clearInterval(myInterval)
+// })
+// let count = document.getElementById('count');
+// let c = 10;
+// let x= setInterval(function ()
+// {
+//     c--;
+//     count.textContent = c;
+//     if (c == 0)
+//     {
+//         clearInterval(x)
+//     }
+// },1000)
+
+// document.getElementById("btn").addEventListener('click', function ()
+// {
+//     clearInterval(x)
+// })
+
+function fun ()
+{
+    console.log("hello world")
+}
+setTimeout(fun, 1);
+for (let i = 0; i < 1000; i++)
+{
+    console.log("bye world")
+}
+for (let i = 0; i < 1000; i++) {
+	console.log("bye world2")
+}
+for (let i = 0; i < 1000; i++) {
+	console.log("bye world3")
+}
